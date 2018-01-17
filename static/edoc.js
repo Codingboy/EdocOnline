e36e2f8630deae6afabc3af7153ae49d2c1097b9
Daniel Tkocz <daniel.tkocz42@gmail.com>
2018-01-17 08:16:37 +0100

    no message

--- static/edoc.js
+++ static/edoc.js
@@ -223,7 +223,8 @@ class SPBox
 		let encoded = new Array(256);
 		for (let i=0; i<256; i++)
 		{
-			encoded[i] = plain[i] ^ this.sBoxes[0].encodeMap[i];
+			encoded[i] = plain[i];
+			//encoded[i] = encoded[i] ^ this.sBoxes[0].encodeMap[i];
 			for (let j=0; j<8; j++)
 			{
 				if ((this.seed[i] & (1<<j)) != 0)
@@ -256,7 +257,7 @@ class SPBox
 					decoded[i] = sBox.decode(decoded[i]);
 				}
 			}
-			decoded[i] = decoded[i] ^ this.sBoxes[0].encodeMap[i];
+			//decoded[i] = decoded[i] ^ this.sBoxes[0].encodeMap[i];
 		}
 		return decoded;
 	}
